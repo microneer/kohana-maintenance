1411edf84c82a1a0140e9a19351fb55b20bdcae5
Michael Fielding <michael.fielding@hawkip.com>
2012-06-19 10:31:44 +1200

    Default window exists now.

--- config/maintenance.php
+++ config/maintenance.php
@@ -1,13 +1,13 @@
 <?php
 
     return array(
-//        'windows'=>array(
+        'windows'=>array(
 //            array(
 //                'from' => '2012-06-18 20:00:00 UTC',
 //                'to' => '2012-06-18 21:10:00 UTC',
 //                'message' => 'This site is undergoing scheduled maintenance from :from until :to and is unavailable during that time.'
 //            )
-//        ),
+        ),
     );
 
 ?>
